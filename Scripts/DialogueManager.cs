6aeab1ab2a653b2a93615406f357fd66b0d6212d
trentdguillen <145140003+trentdguillen@users.noreply.github.com>
2024-04-26 22:46:38 -0400

    Update DialogueManager.cs

--- Scripts/DialogueManager.cs
+++ Scripts/DialogueManager.cs
@@ -2,18 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
+
 public class DialogueManager : MonoBehaviour
 {
-    [SerializeField] GameObject dialogueBox
+    [SerializeField] GameObject dialogueBox;
     [SerializeField] Text dialogueText;
     [SerializeField] int lettersPerSecond;
     public event Action OnShowDialogue;
     public event Action OnHideDialogue;
-    public static DialogueManager Instance {get; private set;}
+    private bool isInventoryShown = false;
+    public static DialogueManager Instance { get; private set; }
     Dialogue dialogue;
     int currentLine = 0;
     bool isTyping;
-    
+
     public IEnumerator ShowDialogue(Dialogue dialogue)
     {
         yield return new WaitForEndOfFrame();
@@ -28,10 +32,10 @@ public class DialogueManager : MonoBehaviour
         isTyping = true;
         dialogueText.text = "";
 
-        foreach(var letter in line.ToCharArray())
+        foreach (var letter in line.ToCharArray())
         {
             dialogueText.text += letter;
-            yield return new WaitForSeconds(1f/lettersPerSecond);
+            yield return new WaitForSeconds(1f / lettersPerSecond);
         }
         isTyping = false;
     }
@@ -41,20 +45,96 @@ public class DialogueManager : MonoBehaviour
         Instance = this;
     }
 
-    private void HandleUpdate()
+    public void HandleUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Z) && !isTyping)
         {
+
             if (++currentLine < dialogue.Lines.Count)
             {
                 StartCoroutine(TypeDialogue(dialogue.Lines[currentLine]));
             }
-            else 
+            else
             {
                 dialogueBox.SetActive(false);
                 currentLine = 0;
                 OnHideDialogue?.Invoke();
+
+                if (dialogue.endsGame)  
+                {
+                    EndGame(); 
+                }
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.O))
+            HideInventory();
+    }
+
+    public void ShowInventory()
+    {
+        Debug.Log("Showing Inventory");
+        if (InventoryManager.Instance != null && InventoryManager.Instance.inventory != null)
+        {
+            var items = InventoryManager.Instance.inventory.GetItemList(); 
+            string inventoryText = "Inventory: ";
+            foreach (var item in items)
+            {
+                inventoryText += item.itemType.ToString() + " x" + item.amount + ", ";
+            }
+
+            if (items.Count > 0)
+                inventoryText = inventoryText.TrimEnd(',', ' ');
+            else
+                inventoryText += "empty";
+
+            DisplayInventoryText(inventoryText);
+            isInventoryShown = true;
+        }
+        else
+        {
+            Debug.LogError("No InventoryManager found or no inventory available.");
+        }
+        Debug.Log("Inventory Shown");
+
+        
+    }
+
+    public void ToggleInventoryDisplay()
+    {
+        if (isInventoryShown)
+        {
+            HideInventory();
+        }
+        else
+        {
+            ShowInventory();
+        }
+    }
+
+    private void DisplayInventoryText(string text)
+    {
+        OnShowDialogue?.Invoke();
+        dialogueBox.SetActive(true);
+        dialogueText.text = text; 
+    }
+
+    private void HideInventory()
+    {
+        Debug.Log("Hiding Inventory");
+        dialogueBox.SetActive(false);
+        isInventoryShown = false;
+        OnHideDialogue?.Invoke();
+        Debug.Log("Inventory Hidden");
+    }
+
+    private void EndGame()
+    {
+        Debug.Log("Game Over - The monster has eaten you!");
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
