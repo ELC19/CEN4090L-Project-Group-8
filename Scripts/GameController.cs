5bb735ef02083619b5ecb4168bddc71f99abc3a8
trentdguillen <145140003+trentdguillen@users.noreply.github.com>
2024-04-26 22:48:30 -0400

    Update GameController.cs

--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -1,4 +1,4 @@
-/* Code by Logan Nyquist */
+/* Code by Logan Nyquist And Trent D. Guillen*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +6,19 @@ using UnityEngine;
 //freeroam: walking around
 //dialogue: talking to npc
 //danger: the forgotten one found you!
-public enum GameState {FreeRoam, Dialogue, Danger}
+public enum GameState { FreeRoam, Dialogue, GameOver }
 
-public class GameController : MonoBehaviour 
+public class GameController : MonoBehaviour
 {
     [SerializeField] OGController ogController;
+    [SerializeField] GameObject jumpScareImage; 
+    [SerializeField] float jumpScareDuration = 3f;
     GameState gameState;
+
     private void Start()
     {
         DialogueManager.Instance.OnShowDialogue += () =>
-        {gameState = GameState.Dialogue;};
+        { gameState = GameState.Dialogue; };
         DialogueManager.Instance.OnHideDialogue += () =>
         {
             if (gameState == GameState.Dialogue)
@@ -23,14 +26,48 @@ public class GameController : MonoBehaviour
         };
 
     }
+
     private void Update()
     {
-        if (gameState = GameState.FreeRoam)
+        if (gameState == GameState.FreeRoam)
+        {
             ogController.HandleUpdate();
+            CheckInventoryCompletion();
+        }
 
-        else if (gameState = GameState.Dialogue)
+        else if (gameState == GameState.Dialogue)
             DialogueManager.Instance.HandleUpdate();
-            
-        else if (gameState = GameState.Danger)
+
+        else if (gameState == GameState.GameOver)
+        {
+            //AudioManager.Instance.PlayScareSound();
+        }
+    }
+
+    private void CheckInventoryCompletion()
+    {
+        if (InventoryManager.Instance.CheckCompleteInventory())
+        {
+            StartCoroutine(EndGameSequence());
+        }
+    }
+
+    private IEnumerator EndGameSequence()
+    {
+        gameState = GameState.GameOver;
+        jumpScareImage.SetActive(true);
+        yield return new WaitForSeconds(jumpScareDuration);
+        EndGame();
+    }
+
+    private void EndGame()
+    {
+        jumpScareImage.SetActive(false);
+        Debug.Log("Game Over");
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
